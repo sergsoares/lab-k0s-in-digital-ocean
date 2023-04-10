e87fe674b705f76f696340799a7f771539214453
sergsoares-personal <sergsoares-personal@sergsoaress-MacBook-Air.local>
2023-04-10 20:08:37 -0300

    [TF] Updated variables

--- tf-k8s/variables.tf
+++ tf-k8s/variables.tf
@@ -1,25 +1,47 @@
-variable "do_token" { 
-  sensitive = true
-  type = string 
+variable "name" {
+  type = string
 }
 
-
-variable "ssh_keys" { type = list(any) }
-variable "name" { type = string }
-variable "domain" { type = string }
-
-
-variable "cloudflare_api_token" { 
-  sensitive = true
-  type = string 
+variable "ssh_keys" {
+  type = list(any)
 }
-variable "cloudflare_email" { type = string }
 
-variable "config" { type = string }
+variable "domain" {
+  type = string
+}
+
+variable "cloudflare_api_token" {
+  sensitive = true
+  type      = string
+}
+
+variable "do_config" {
+  type = string
+}
+
+variable "do_token" {
+  sensitive = true
+  type      = string
+}
 
 variable "do_image" {
   type    = string
   default = "ubuntu-22-04-x64"
+}
+
+variable "do_region" {
+  type    = string
+  default = "nyc3"
+}
+
+variable "do_size" {
+  type    = string
+  default = "s-1vcpu-1gb"
+}
+
+variable "k0s_port" {
+  type    = number
+  default = 22
 }
 
 variable "k0s_host_user" {
@@ -31,22 +53,8 @@ variable "k0s_keypath" {
   type    = string
   default = "~/.ssh/id_rsa.pub"
 }
-variable "k0s_port" {
-  type    = number
-  default = 22
-}
 
-variable "do_region" {
-  type    = string
-  default = "nyc3"
-}
-variable "do_size" {
-  type    = string
-  default = "s-1vcpu-1gb"
-}
-
-variable "k8s_version" {
+variable "k0s_kubernetes_version" {
   type    = string
   default = "1.23.8+k0s.0"
 }
-
